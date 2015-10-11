06fafe5843ab1f010b94f26fb183f9ffff0a66f7
baskezada <baskezada@gmail.com>
2015-10-11 17:59:44 -0300

    Nuevo con parrafos de plagio

--- Plagio.py
+++ Plagio.py
@@ -31,6 +31,7 @@ def matriztexto():
             continue
         count+=1
     return [[i]*count for i in range(count)]
+
 def matrizfinal(matriz):
     for i in range(len(matriz)):
         for x in range(len(matriz[i])):
@@ -79,21 +80,55 @@ for linea in arch:
         matrizcos[nlinea-1][nlinea2-1]= round(dcos(h1,h2),2)
         matrizfinal(matrizcos)
         matrizcos= array(matrizcos)
+        
         lista.append(round(deucli(h1,h2),2))
         lista2.append(round(dcos(h1,h2),2))
         nlinea2+=1
     arch2.close()
     nlinea+=1
                 
-print matrizeu ,"\n", matrizcos
 arch.close()
+print matrizeu
+print matrizcos
 
-
+#Pedimos el rango de distancia en el que se encuentra el plagio analizando los graficos.
+print "ingrese el intervalo en el que se encuentra el plagio a partir del grafico de distancia euclidiana"
+menor1 = float(raw_input("Limite menor: "))
+mayor1 = float(raw_input("Limite mayor: "))
+plagio_euc = []
+for a in range(len(matrizeu)):
+    for x in range(len(matrizeu[a])):
+        if matrizeu[a][x]>= menor1 and matrizeu[a][x]<= mayor1:
+            plagio_euc.append((a+1,x+1))
+            for parrafo in plagio_euc:
+                e,b=parrafo
+                for parrafo2 in plagio_euc:
+                    c,d=parrafo2
+                    if e == d and b==c:
+                       del plagio_euc[plagio_euc.index((c,d))]
+               
+for x in plagio_euc:
+    par1, par2 =x
+    print "El parrafo " + str(par1) + " con el parrafo " + str(par2) +" pareciera ser plagio."
     
+print "ingrese el intervalo en el que se encuentra el plagio a partir del grafico de distancia coseno"
+menor2 = float(raw_input("Limite menor: "))
+mayor2 = float(raw_input("Limite mayor: "))
+plagio_cos = []
+for a in range(len(matrizcos)):
+    for x in range(len(matrizcos[a])):
+        if matrizcos[a][x]>= menor2 and matrizcos[a][x]<= mayor2:
+            plagio_cos.append((a+1,x+1))
+            for parrafo in plagio_cos:
+                e,b=parrafo
+                for parrafo2 in plagio_cos:
+                    c,d=parrafo2
+                    if e == d and b==c:
+                       del plagio_cos[plagio_cos.index((c,d))]
 
-
-
-
+for x in plagio_cos:
+    par1, par2 = x
+    print "El parrafo " + str(par1) + " con el parrafo " + str(par2) +" pareciera ser plagio."
 
 
 
